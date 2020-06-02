e06c6a18d87a22333eb10f0cf0bf6731293b15b9
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-02 12:20:48 -0700

    add first two functions ConvCels & reverseStr

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
-// Your functions go here!
+// Convert Celsius to Fahrenheit using equation
+// A1: celsius
+// R: fahrenheit
+function convertCelsToF(celsius) {
+   let fahrenheit = celsius * (9 / 5) + 32;
+   return fahrenheit;
+}
+
+//Reverse a string
+//A1: str
+//R: reverse string
+function reverseStr(str) {
+   //split given string str by char using .splt
+   let split = str.split("");
+   //use .reverse to reverse split string
+   let reverse = split.reverse();
+   //join char back together .join
+   return reverse.join("");
+}
